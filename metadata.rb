275371c298c0170d45a474b242025e0c1ea226c0
Alex Ormenisan <ormenisan.adrian@gmail.com>
2019-12-16 12:06:59 +0100

    fix

--- metadata.rb
+++ metadata.rb
@@ -1932,7 +1932,7 @@ attribute "hopsworks/provenance",
           :description => "MIN provenance - community edition. FULL provenance - enterprise",
           :type => 'string'
 
-attribute "hopsworks/provenance/archive_batch_size",
+attribute "hopsworks/provenance/archive/batch_size",
           :description => "Provenance cleaning size per round. Number of cleaned indices(per project)",
           :type => 'string'
 

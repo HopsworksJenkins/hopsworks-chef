0da963fd2d79b6813e12114735e59b0f28d886ec
Theofilos Kakantousis <t.kakantousis@gmail.com>
2020-10-08 15:35:05 +0200

    Create rules and fg_rules tables

--- files/default/sql/ddl/updates/undo/2.2.0__undo.sql
+++ files/default/sql/ddl/updates/undo/2.2.0__undo.sql
@@ -1,2 +1,7 @@
 ALTER TABLE `hopsworks`.`feature_store_tag` ADD COLUMN `type` varchar(45) NOT NULL DEFAULT 'STRING';
 ALTER TABLE `hopsworks`.`feature_store_tag` DROP COLUMN `tag_schema`;
+DROP TABLE IF EXISTS `validation_rule`;
+DROP TABLE IF EXISTS `feature_group_rule`;
+DROP TABLE IF EXISTS `feature_group_validation`;
+DROP TABLE IF EXISTS `feature_store_expectation_rule`;
+ALTER TABLE `hopsworks`.`feature_group` DROP COLUMN `validation_type`;

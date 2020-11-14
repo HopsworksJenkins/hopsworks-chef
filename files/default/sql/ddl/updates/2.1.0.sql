20508e3ab8560a4c4e85774b9faad13b90d34844
robzor92 <robin.eric.andersson@gmail.com>
2020-11-14 15:30:17 +0100

    [HOPSWORKS-2131] Add pip check command to Python service

--- files/default/sql/ddl/updates/2.1.0.sql
+++ files/default/sql/ddl/updates/2.1.0.sql
@@ -8,3 +8,20 @@ EXECUTE stmt1;
 DEALLOCATE PREPARE stmt1;
 
 ALTER TABLE `hopsworks`.`conda_commands` CHANGE `environment_yml` `environment_file` VARCHAR(1000) COLLATE latin1_general_cs DEFAULT NULL;
+
+ALTER TABLE `hopsworks`.`project` ADD COLUMN `python_env_id` int(11) DEFAULT NULL;
+
+CREATE TABLE IF NOT EXISTS `python_environment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `project_id` int(11) NOT NULL,
+  `python_version` VARCHAR(25) COLLATE latin1_general_cs NOT NULL,
+  `jupyter_conflicts` TINYINT(1) NOT NULL DEFAULT '0',
+  `conflicts` VARCHAR(12000) COLLATE latin1_general_cs DEFAULT NULL,
+  UNIQUE KEY `project_env` (`project_id`),
+  PRIMARY KEY (`id`),
+  CONSTRAINT `FK_PROJECT_ID` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=ndbcluster AUTO_INCREMENT=119 DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+
+ALTER TABLE `hopsworks`.`project` DROP COLUMN `conda`;
+
+ALTER TABLE `hopsworks`.`project` DROP COLUMN `python_version`;

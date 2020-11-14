6850285df427c2141caa26ac0f1d1574dfe40613
robzor92 <robin.eric.andersson@gmail.com>
2020-11-14 15:30:17 +0100

    [HOPSWORKS-2131] Add pip check command to Python service

--- files/default/sql/ddl/updates/undo/2.1.0__undo.sql
+++ files/default/sql/ddl/updates/undo/2.1.0__undo.sql
@@ -1,6 +1,7 @@
 ALTER TABLE `hopsworks`.`conda_commands` ADD CONSTRAINT `FK_284_520` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 ALTER TABLE `hopsworks`.`conda_commands` CHANGE `environment_file` `environment_yml` VARCHAR(1000) COLLATE latin1_general_cs DEFAULT NULL;
+
 ALTER TABLE `hopsworks`.`feature_store_jdbc_connector` DROP INDEX `jdbc_connector_feature_store_id_name`;
 ALTER TABLE `hopsworks`.`feature_store_s3_connector` DROP INDEX `s3_connector_feature_store_id_name`;
 ALTER TABLE `hopsworks`.`feature_store_s3_connector` DROP INDEX `fk_feature_store_s3_connector_1_idx`;
@@ -10,3 +11,12 @@ ALTER TABLE `hopsworks`.`feature_store_s3_connector` DROP COLUMN `key_secret_uid
 ALTER TABLE `hopsworks`.`feature_store_s3_connector` DROP COLUMN `key_secret_name`;
 
 DROP TABLE IF EXISTS `hopsworks`.`feature_store_redshift_connector`;
+
+ALTER TABLE `hopsworks`.`python_environment` DROP FOREIGN KEY `FK_PROJECT_ID`;
+
+ALTER TABLE `hopsworks`.`project` DROP COLUMN `python_env_id`;
+
+DROP TABLE IF EXISTS `hopsworks`.`python_environment`;
+
+ALTER TABLE `hopsworks`.`project` ADD COLUMN `conda` tinyint(1) DEFAULT '0';
+ALTER TABLE `hopsworks`.`project` ADD COLUMN `python_version` varchar(25) COLLATE latin1_general_cs DEFAULT NULL;

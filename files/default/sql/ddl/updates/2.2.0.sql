3f72dfaba61019595ed9723aa37a0f976d8bd57e
Javier de la Rúa Martínez <javierdlrm@outlook.com>
2021-02-01 10:45:59 +0100

    Add serving tool and model server concepts

--- files/default/sql/ddl/updates/2.2.0.sql
+++ files/default/sql/ddl/updates/2.2.0.sql
@@ -9,3 +9,6 @@ ALTER TABLE `users` DROP COLUMN `security_question`, DROP COLUMN `security_answe
 
 ALTER TABLE `hopsworks`.`feature_store_tag` DROP COLUMN `type`;
 ALTER TABLE `hopsworks`.`feature_store_tag` ADD COLUMN `tag_schema` VARCHAR(13000) NOT NULL DEFAULT '{"type":"string"}';
+
+ALTER TABLE `hopsworks`.`serving` ADD COLUMN `serving_tool` INT(11) NOT NULL DEFAULT '0';
+ALTER TABLE `hopsworks`.`serving` RENAME COLUMN `serving_type` TO `model_server`;

bb5c1939d372736bddcefe50f1afdeb626c3b5ff
moritzmeister <meister.mo@gmail.com>
2021-04-22 13:28:18 +0200

    add cached_feature table

--- files/default/sql/ddl/updates/2.3.0.sql
+++ files/default/sql/ddl/updates/2.3.0.sql
@@ -1,2 +1,12 @@
 ALTER TABLE `hopsworks`.`serving` ADD COLUMN `deployed` timestamp DEFAULT NULL;
 ALTER TABLE `hopsworks`.`serving` ADD COLUMN `revision` VARCHAR(8) DEFAULT NULL;
+
+CREATE TABLE `cached_feature` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cached_feature_group_id` int(11) NULL,
+  `name` varchar(63) COLLATE latin1_general_cs NOT NULL,
+  `description` varchar(256) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  KEY `cached_feature_group_fk` (`cached_feature_group_id`),
+  CONSTRAINT `cached_feature_group_fk2` FOREIGN KEY (`cached_feature_group_id`) REFERENCES `cached_feature_group` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
--- files/default/sql/ddl/updates/undo/2.3.0__undo.sql
+++ files/default/sql/ddl/updates/undo/2.3.0__undo.sql
@@ -1,2 +1,4 @@
 ALTER TABLE `hopsworks`.`serving` DROP COLUMN `deployed`;
 ALTER TABLE `hopsworks`.`serving` DROP COLUMN `revision`;
+
+DROP TABLE `hopsworks`.`cached_feature`;
